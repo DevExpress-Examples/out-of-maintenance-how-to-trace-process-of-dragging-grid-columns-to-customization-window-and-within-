51089188510df5f3b0e1e6c33b27b6a0b7a99688
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void grid_AfterPerformCallback(object sender, ASPxGridViewAfterPerformCallbackEventArgs e) {
+        if (e.CallbackName == "COLUMNMOVE") {
+            ASPxGridView gridView = (ASPxGridView)sender;
+            if ((gridView.Columns[hf["fieldName"].ToString()]).Visible)
+                gridView.JSProperties["cpMoveTo"] = "Header";
+            else
+                gridView.JSProperties["cpMoveTo"] = "Customization Window";
+        }
+    }
+}
